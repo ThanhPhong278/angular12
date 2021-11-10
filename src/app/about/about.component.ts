79844bb39ba446218c9cf81957dd192981a63a78
ThanhPhong278 <phongvo2728@gmail.com>
2021-11-10 11:34:38 +0700

    Lear Angular *ngFor, *ngif, *ngClass

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
   public TraiCay = ['Táo','Nho','Cam','Xoài','Bưởi'];
+  public TraiCay2 = [
+    {ten: 'Táo',gia: 12, hagia: true}, 
+    {ten: 'Nho',gia: -3, hagia: false},
+    {ten: 'Cam',gia: 5, hagia: true},
+  ];
   constructor() { }
 
   public  ngOnInit(): void {
-    console.log('trai cay= ', this.TraiCay);
+    console.log('trai cay= ', this.TraiCay2);
   }
 
 }
